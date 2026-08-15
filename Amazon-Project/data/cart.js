ddb6eec589f3eb9284b010bdf5dd5bc4dc318c56
Ronald <okartercu@gmail.com>
2026-08-15 16:26:23 +0100

    update cart quantity, checkout page done halfway

--- Amazon-Project/data/cart.js
+++ Amazon-Project/data/cart.js
@@ -1,22 +1,49 @@
-export const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
+  {
+    productId: "id001",
+    quantity: 2
+  },
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 1
+  }
+];
 
 export function addToCart(productId) {
-     let matchingItem;
-     const selectorValue = document.querySelector(`.js-quantity-selector-${productId}`);
+	let matchingItem;
+	const selectorValue = document.querySelector(`.js-quantity-selector-${productId}`);
 
-      cart.forEach((cartItem) => {
-       if (productId === cartItem.productId) {
-            matchingItem = cartItem;
-        }});
+	cart.forEach((cartItem) => {
+		if (productId === cartItem.productId) {
+				matchingItem = cartItem;
+		}});
 
-        let quantity = Number(selectorValue.value);
-        
-        if (matchingItem) {
-          matchingItem.quantity+= quantity;
-        } else {
-          cart.push({
-          productId,
-          quantity
-         });         
-       }
+		let quantity = Number(selectorValue.value);
+		
+		if (matchingItem) {
+			matchingItem.quantity+= quantity;
+		} else {
+			cart.push({
+			productId,
+			quantity
+			});         
+  	}
+		localStorage.setItem('cart', JSON.stringify(cart));
   }
+
+
+
+  
+export function removeFromCart(productId) {
+	let newCart = [];
+	const container = document.querySelector(`.js-container-${productId}`);
+	cart.forEach((cartItem) => {
+		let matchingItem;
+		if (productId !== cartItem.productId) {
+			newCart.push(cartItem);
+		}
+	})
+	cart = newCart;
+	localStorage.setItem('cart', JSON.stringify(cart));
+	container.remove();
+}
